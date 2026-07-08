56a19eba203a1bee9b6ace28f72cc32dfe00c8ab
mob99-9 <jamesorozco20050@gmail.com>
2026-07-08 17:08:57 +0800

    fix: update legacy routes schema to modern vercel rewrites structure

--- api/index.php
+++ api/index.php
@@ -1,7 +1,3 @@
 <?php
 
-// Force the working directory back to the repository root root
-chdir(__DIR__ . '/..');
-
-// Explicitly register Laravel's core public handler
 require __DIR__ . '/../public/index.php';
